7ad89d7866e308fb2034bba92c24dcf2e8c07111
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-05-09 20:17:34 -0400

    gamerunner while loop channged

--- src/homework/GameRunner.java
+++ src/homework/GameRunner.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class GameRunner {
 
-	private static boolean notAWinner;
+	private static boolean running; //game running bool
 
 	public static void main(String[] args) 
         {
@@ -15,21 +15,21 @@ public class GameRunner {
 		aGame.add("Pat");
 		aGame.add("Sue");
 		
-/*		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-*/		
+                Random rand = new Random();
+                
+                while (running)
+                {
+                    if (rand.nextInt(9) == 7) 
+                    {
+                            //notAWinner = aGame.wrongAnswer();
+                    } 
+                    else 
+                    {
+                            //end the game
+                            //notAWinner = aGame.wasCorrectlyAnswered();
+                            running = false;
+                    }
+
+                }
 	}
 }
