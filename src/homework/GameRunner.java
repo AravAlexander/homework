7ee6242c1cb500dc0eec64bad03836c927207126
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-05-09 20:32:07 -0400

    updated gamerunner comment

--- src/homework/GameRunner.java
+++ src/homework/GameRunner.java
@@ -21,12 +21,16 @@ public class GameRunner {
                 {
                     aGame.roll(rand.nextInt(5) + 1);
                     
+                    //They roll a 10 sided dice here and if they get a 7 it runs
+                    // them into the penalty box
                     if (rand.nextInt(9) == 7) 
                     {
                             //notAWinner = aGame.wrongAnswer();
                     } 
                     else 
                     {
+                        //if they got a 7 on the 10 sided die and they have the
+                        //correct answer they seem to win
                             //end the game
                             //notAWinner = aGame.wasCorrectlyAnswered();
                             running = false;
